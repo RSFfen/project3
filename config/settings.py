c40f05df4d9fdff7af7483a877a2cc7fe7083566
RSFfen <rsf61.fen@gmail.com>
2021-06-11 19:16:39 +0300

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT= 'static'
+
+#STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, 'static')
+#]
