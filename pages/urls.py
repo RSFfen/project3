54f452b806157ed775a58347b48ed98c750bdd60
RSFfen <rsf61.fen@gmail.com>
2021-06-12 13:15:42 +0300

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import HomePageView, AboutPageView # new
 from .views import CarsPageView # new
 from .views import HelpPageView # new
+from .views import CommentCreateView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'), # new
     path('', HomePageView.as_view(), name='home'),
     path('cars/', CarsPageView.as_view(), name='cars'), # new
     path('help/', HelpPageView.as_view(), name='help'), # new
+    path('comment_new/', CommentCreateView.as_view(), name='comment_new'),    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView 
+from .models import Comment
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    context_object_name = 'all_comments_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_comments_list'] = Comment.objects.all()
+        return context
 
 class AboutPageView(TemplateView): # new
     template_name = 'about.html'
@@ -13,3 +19,8 @@ class CarsPageView(TemplateView): # new
     
 class HelpPageView(TemplateView): # new
     template_name = 'help.html'
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = 'comment_new.html'
+    fields = ('text',)
